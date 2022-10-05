89ef32ace38fab93c9cb7fdd9a8f0693fa7a7d8f
houssem janfaoui <81567436+hoss90@users.noreply.github.com>
2022-10-05 11:28:28 +0100

    Update index.jsx

--- src/Pages/Skills/index.jsx
+++ src/Pages/Skills/index.jsx
@@ -10,16 +10,16 @@ function Skills() {
                 My Skillsets
             </h1>
             <div className=' text-rose-300 -z-50'>
-                <img src={html} alt="html logo" className='w-32 md:w-28 sm:w-20 inline-block absolute top-56 left-10 md:top-96 sm:top-2/3 opacity-70' />
-                <img src={css} alt="css logo"  className='w-32 md:w-28 sm:w-20 inline-block absolute top-56 right-10 md:top-2/3 opacity-70'/>
-                <img src={react} alt="react logo"  className='w-32 md:w-28 sm:w-20 inline-block absolute top-32 left-1/2 sm:top-1/2 opacity-70'/>
-                <img src={tailwind} alt="tailwind logo"  className='w-32 inline-block absolute bottom-4 left-1/2 opacity-70'/>
-                <span className='absolute top-28 left-20 sm:top-1/4 sm:left-12 lg:text-3xl md:text-xl sm:text-lg font-bold opacity-70'>HTML5</span>
-                <span className='absolute top-48 left-1/4 sm:top-1/2 lg:text-5xl md:text-2xl sm:text-xl font-bold opacity-50'>ReactJs</span>
-                <span className='absolute top-96 right-48 md:top-1/2 sm:left-3/4 text-2xl md:text-xl sm:text-lg font-bold opacity-70'>CSS3</span>
-                <span className='absolute top-28 right-20 sm:top-40 sm:right-10 text-2xl md:text-xl sm:text-lg font-bold opacity-60'>NodeJs</span>
-                <span className='absolute bottom-32 right-96 md:bottom-80 sm:right-10 sm:bottom-36 text-2xl md:text-xl sm:text-lg font-bold opacity-90'>TailwindCSS</span>
-                <span className='absolute bottom-28 left-20 text-xl md:text-xl sm:text-lg font-bold opacity-50'>Figma</span>
+                <img src={html} alt="html logo" className='w-32 md:w-28 sm:w-20 inline-block absolute top-56 left-10 md:top-96 sm:top-2/3 opacity-70 -z-10' />
+                <img src={css} alt="css logo"  className='w-32 md:w-28 sm:w-20 inline-block absolute top-56 right-10 md:top-2/3 opacity-70 -z-10'/>
+                <img src={react} alt="react logo"  className='w-32 md:w-28 sm:w-20 inline-block absolute top-32 left-1/2 sm:top-1/2 opacity-70 -z-10'/>
+                <img src={tailwind} alt="tailwind logo"  className='w-32 inline-block absolute bottom-4 left-1/2 opacity-70 -z-10'/>
+                <span className='absolute top-28 left-20 sm:top-1/4 sm:left-12 lg:text-3xl md:text-xl sm:text-lg font-bold opacity-70 -z-10'>HTML5</span>
+                <span className='absolute top-48 left-1/4 sm:top-1/2 lg:text-5xl md:text-2xl sm:text-xl font-bold opacity-50 -z-10'>ReactJs</span>
+                <span className='absolute top-96 right-48 md:top-1/2 sm:left-3/4 text-2xl md:text-xl sm:text-lg font-bold opacity-70 -z-10'>CSS3</span>
+                <span className='absolute top-28 right-20 sm:top-40 sm:right-10 text-2xl md:text-xl sm:text-lg font-bold opacity-60 -z-10'>NodeJs</span>
+                <span className='absolute bottom-32 right-96 md:bottom-80 sm:right-10 sm:bottom-36 text-2xl md:text-xl sm:text-lg font-bold opacity-90 -z-10'>TailwindCSS</span>
+                <span className='absolute bottom-28 left-20 text-xl md:text-xl sm:text-lg font-bold opacity-50 -z-10'>Figma</span>
             </div>
         </div>
     )
